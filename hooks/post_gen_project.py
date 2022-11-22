6cc28fd8ec087f2508d12bf63e89d33d67afc991
Peter Mason <pm3.141@gmail.com>
2022-11-21 19:41:24 -0800

    Add poetry and pyproject.toml to base project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,19 +1,23 @@
+"""
+Post-project-creation hooks.
+
+After successfully creating the cookiecutter and validating it with the pre-generation hooks, set up the new project
+ environment by installing Python, Poetry, and other dev dependencies.
+"""
+
 import os
-import subprocess
 import re
+import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
-def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
-
-
 def install_python_and_deps():
-    python_version =  "{{ cookiecutter.python_version }}"
+    """Automatically install the specified version of Python as well as other standard development tooling."""
+    python_version = "{{ cookiecutter.python_version }}"
     if python_version == "latest":
-        standard_version_regex =  r"^3.[0-9]+.[0-9]+$"
-        subprocess.run(["pyenv", "update"]) # Make sure most recent Python versions are accessible
+        standard_version_regex = r"^3.[0-9]+.[0-9]+$"
+        subprocess.run(["pyenv", "update"])  # Make sure most recent Python versions are accessible
         cmd_output = subprocess.run(["pyenv", "install", "--list"], capture_output=True, encoding="UTF-8")
         all_versions = cmd_output.stdout.split("\n")
         all_versions = map(lambda x: x.strip(), all_versions)
@@ -26,11 +30,10 @@ def install_python_and_deps():
     subprocess.run(["pip", "install", "poetry"])
     subprocess.run(["poetry", "install"])
     subprocess.run(["git", "init"])
-
+    subprocess.run(["pre-commit", "install"])
 
 
 if __name__ == "__main__":
     if "{{ cookiecutter.license }}" == "Not Open Source":
-        remove_file("LICENSE")
+        os.remove(os.path.join(PROJECT_DIRECTORY, "LICENSE"))
     install_python_and_deps()
-
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,9 +1,17 @@
+"""
+Pre-project-creation hooks.
+
+After all arguments to the cookiecutter project template have been given, validate the inputs.  If any fail, abort the
+ creation process and clean up any created resources.
+"""
+
 import re
-import sys
 import subprocess
+import sys
 
 
 def validate_project_slug():
+    """Validate that the project slug given during project creation can be a proper Python module name."""
     valid_module_regex = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
     module_name = "{{ cookiecutter.project_slug}}"
 
@@ -18,6 +26,7 @@ def validate_project_slug():
 
 
 def validate_python_version():
+    """Validate that the version of Python given during project creation.  Only accepts standard CPython versions."""
     python_version = "{{ cookiecutter.python_version }}"
     valid_version_regex = r"^(latest|3|3.[0-9]+|3.[0-9]+.[0-9]+)$"
 
@@ -31,15 +40,14 @@ def validate_python_version():
 
 
 def verify_pyenv_installed():
-    check_pyenv_command = "pyenv -v"
+    """Confirm that pyenv, a prerequisite for this template, has already been installed."""
     try:
         subprocess.run(["pyenv", "-v"], capture_output=True, encoding="UTF-8")
 
-    except(FileNotFoundError):
+    except (FileNotFoundError):
         print("Pyenv was not found.  Please follow the instructions for installing requirements.")
         # Cancel project creation
         sys.exit(1)
-
 
 
 validate_project_slug()
