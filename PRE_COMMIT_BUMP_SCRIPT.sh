9ae82fe4201a59e389553f84219a7c905c1d7d34
Peter Mason <pm3.141@gmail.com>
2023-11-23 07:46:39 -0800

    Fix auto-bumping for ruff vs. ruff-pre-commit

--- PRE_COMMIT_BUMP_SCRIPT.sh
+++ PRE_COMMIT_BUMP_SCRIPT.sh
@@ -3,12 +3,19 @@
  #  pre-commit releases apart from the initial `v` (e.g. `v1.2.3` vs. `1.2.3`)
 
 # Pre-commit dependency list
-deps=(pre-commit-hooks poetry creosote absolufy-imports codespell black blacken-docs ruff)
+# deps=(pre-commit-hooks poetry creosote absolufy-imports codespell black blacken-docs ruff-pre-commit)\
+deps=(ruff)
 pc_path="{{ cookiecutter.project_slug }}/.pre-commit-config.yaml"
 
 for hook in ${deps[@]}
 do
     python -m pip install --upgrade pip >/dev/null 2>&1
     version=$(python -m pip index versions $hook 2>/dev/null | grep -Po '\(\K([\d|\.]+)')
+    # ruff-pre-commit doesn't have a PyPI package but instead matches the ruff version number for its releases
+    if [ $hook = "ruff" ]; then
+        hook="ruff-pre-commit"
+    fi
+    echo $version
+    echo $hook
     perl -0777pi -e "s/(?<=- repo: https:\/\/github.com\/.{1,50}$hook\s{1,10}rev: v?)([\d|\.]{1,20})/$version/g" "$pc_path" >/dev/null 2>&1
 done
