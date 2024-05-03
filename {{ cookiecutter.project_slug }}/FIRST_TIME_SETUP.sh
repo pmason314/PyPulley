0d2be947ec34ca6da4d3beacf1010694fb8504e6
Peter Mason <peter@petermason.me>
2024-05-03 00:54:41 -0700

    Add create=true setting to poetry.toml

--- {{ cookiecutter.project_slug }}/FIRST_TIME_SETUP.sh
+++ {{ cookiecutter.project_slug }}/FIRST_TIME_SETUP.sh
@@ -1,8 +1,9 @@
 $(pyenv which python) -m pip install --upgrade pip >/dev/null
 $(pyenv which python) -m pip install pipx >/dev/null
 $(pyenv which python) -m pipx reinstall poetry >/dev/null 2>&1
-poetry config --local virtualenvs.prefer-active-python true >/dev/null
+poetry config --local virtualenvs.create true >/dev/null
 poetry config --local virtualenvs.in-project true >/dev/null
+poetry config --local virtualenvs.prefer-active-python true >/dev/null
 poetry self update >/dev/null
 poetry self add poetry-dotenv-plugin >/dev/null
 poetry add --lock --group dev black ruff pytest pytest-cov pytest-mock >/dev/null
